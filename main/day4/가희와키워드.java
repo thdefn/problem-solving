ef4a8f3bd887894be2a85e5971c0931950727d49
haden <80521474+thdefn@users.noreply.github.com>
2024-07-20 11:41:56 +0900

    day4

---
+++ main/day4/가희와키워드.java
@@ -0,0 +1,35 @@
+package main.day4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class 가희와키워드 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 1. 메모장 키워드는 set 에 전부 추가
+        Set<String> keywordSet = new HashSet<>();
+        while (N-- > 0) {
+            keywordSet.add(br.readLine());
+        }
+
+
+        // 2. 블로그 키워드를 set 에서 지움
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine(), ",");
+            while (st.hasMoreTokens()) {
+                keywordSet.remove(st.nextToken());
+            }
+            System.out.println(keywordSet.size());
+        }
+
+        br.close();
+    }
+}
---
+++ main/day4/서로다른부분문자열.java
@@ -0,0 +1,27 @@
+package main.day4;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class 서로다른부분문자열 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String S = sc.next();
+        char[] ch = S.toCharArray();
+
+        Set<String> set = new HashSet<>();
+        // 1. i번째 문자로 시작한는 문자열을 순차적으로 set 에 저장
+        for (int i = 0; i < ch.length; i++) {
+            StringBuilder sb = new StringBuilder(Character.toString(ch[i]));
+            set.add(sb.toString());
+            for (int j = i + 1; j < ch.length; j++) {
+                sb.append(ch[j]);
+                set.add(sb.toString());
+            }
+        }
+
+        // 2. set 의 크기 출력
+        System.out.println(set.size());
+    }
+}

6ee01461ef4d223463d4b0d3568681f560850367
haden <80521474+thdefn@users.noreply.github.com>
2024-09-13 10:54:33 +0900

    [day2]

---
+++ columbus/day2/카드놓기.java
@@ -0,0 +1,47 @@
+package columbus.day2;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ * 1. 입력 : 첫째 줄에 n, 둘째 줄에 k, 셋째줄부터  n개 줄 카드에 적혀있는 수
+ * <p>
+ * 2.
+ * 3. 출력
+ */
+public class 카드놓기 {
+    static int k;
+    static Set<Integer> numberSet = new HashSet<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        k = sc.nextInt();
+
+        int[] cards = new int[n];
+        for (int i = 0; i < n; i++) {
+            cards[i] = sc.nextInt();
+        }
+
+        dfs(cards, new boolean[n], 0, "");
+
+
+        System.out.println(numberSet.size());
+    }
+
+    static void dfs(int[] cards, boolean[] selected, int depth, String number) {
+        if (depth == k) {
+            numberSet.add(Integer.parseInt(number));
+            return;
+        }
+
+        for (int i = 0; i < cards.length; i++) {
+            if (selected[i])
+                continue;
+            selected[i] = true;
+            dfs(cards, selected, depth + 1, number + cards[i]);
+            selected[i] = false;
+        }
+    }
+}
---
+++ columbus/day2/퇴사.java
@@ -0,0 +1,33 @@
+package columbus.day2;
+
+import java.util.Scanner;
+
+public class 퇴사 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] T = new int[N];
+        int[] P = new int[N];
+        for (int i = 0; i < N; i++) {
+            T[i] = sc.nextInt();
+            P[i] = sc.nextInt();
+        }
+
+
+        System.out.println(dfs(T, P, 0, 0));
+
+
+    }
+
+    static int dfs(int[] T, int[] P, int index, int totalProfit) {
+        int maxProfit = totalProfit;
+        for (int i = index; i < T.length; i++) {
+            // 1. 재직 기간 내에 상담을 할 수 없다면, skip
+            if (i + T[i] > T.length) continue;
+            // 2. 재직 기간 내에 상담을 할 수 있다면
+            maxProfit = Math.max(dfs(T, P, i + T[i], totalProfit + P[i]), maxProfit);
+        }
+
+        return maxProfit;
+    }
+}
